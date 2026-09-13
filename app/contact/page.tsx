06b102ba9b45f8cc313efacd3927db28f06fe40f
Kayomarz Pavri <66328031+Kayo2970@users.noreply.github.com>
2026-09-14 03:22:49 +0530

    Format campus location with line break

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -66,7 +66,10 @@ export default function ContactPage() {
                       </div>
                       <div>
                         <div className="font-bold text-[#1E0C3D]">Campus Location</div>
-                        <div className="text-slate-600">Faculty of Management and Commerce, RUAS M. S. Ramaiah University of Applied Sciences, Gnanagangothri Campus, Bengaluru 560054</div>
+                        <div className="text-slate-600">
+                          Faculty of Management and Commerce, RUAS<br />
+                          M. S. Ramaiah University of Applied Sciences, Gnanagangothri Campus, Bengaluru 560054
+                        </div>
                       </div>
                     </div>
 
